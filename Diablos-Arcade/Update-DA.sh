852494ddc6cbde0aefda190f68b46ff516f15910
Retro-Devils <88673522+Retro-Devils@users.noreply.github.com>
2022-01-27 10:25:32 -0500

    Update Update-DA.sh

--- Diablos-Arcade/Update-DA.sh
+++ Diablos-Arcade/Update-DA.sh
@@ -3,7 +3,8 @@ function main_menu() {
 local choice 
 while true; do 
 choice=$(dialog --backtitle "$BACKTITLE" --title "DIABLOS ARCADE UPDATER " \ 
---ok-label Select --cancel-label Exit \ --menu "V1.0 TO V1.1" 20 50 30 \ 
+--ok-label Select --cancel-label Exit \ 
+--menu "V1.0 TO V1.1" 20 50 30 \ 
 1 "Update Diablos Arcade " \ 
 2 "View Changelog" \ 
 2>&1 >/dev/tty) 
